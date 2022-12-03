91a33fcc645d82c3c1ccf139f6626c41df06893d
atomlg <58149549+atomlg@users.noreply.github.com>
2022-12-03 08:40:27 +0300

    Update Interactor.cs

--- Assets/_Project/Scripts/Interactor.cs
+++ Assets/_Project/Scripts/Interactor.cs
@@ -1,11 +1,25 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class Interactor : MonoBehaviour
 {
+    private const float UpdateInteractableAfterSeconds = 0.2f;
+    
     [SerializeField] private float _minimumDistance;
     
     private Interactable _nearestInteractable;
-    
+    private Interactable[] _allInteractables;
+    private WaitForSeconds _updateInteractableAfter;
+
+    private void Awake()
+    {
+        _allInteractables = FindObjectsOfType<Interactable>();
+        _updateInteractableAfter = new WaitForSeconds(UpdateInteractableAfterSeconds);
+    }
+
+    private void Start() => StartCoroutine(UpdateNearestInteractableCoroutine());
+
     private void OnInteract()
     {
         if(_nearestInteractable == null)
@@ -13,21 +27,25 @@ public class Interactor : MonoBehaviour
         
         _nearestInteractable.Interact();
     }
-
-    private void Update()
+    
+    private IEnumerator UpdateNearestInteractableCoroutine()
     {
-        _nearestInteractable = null;
-        Interactable[] interactables = FindObjectsOfType<Interactable>();
-        for (int i = 0; i < interactables.Length; i++)
+        for (;;)
         {
-            Interactable interactable = interactables[i];
-            if (Distance(interactable.transform) > _minimumDistance)
-                continue;
+            _nearestInteractable = null;
+            for (int i = 0; i < _allInteractables.Length; i++)
+            {
+                Interactable interactable = _allInteractables[i];
+                if (Distance(interactable.transform) > _minimumDistance)
+                    continue;
 
-            if (_nearestInteractable == null || Distance(interactable.transform) < Distance(_nearestInteractable.transform))
-                _nearestInteractable = interactable;
+                if (_nearestInteractable == null || Distance(interactable.transform) < Distance(_nearestInteractable.transform))
+                    _nearestInteractable = interactable;
+            }
+
+            yield return _updateInteractableAfter;
         }
     }
-
-    public float Distance(Transform other) => Vector3.Distance(transform.position, other.position);
+    
+    private float Distance(Transform other) => Vector3.Distance(transform.position, other.position);
 }
